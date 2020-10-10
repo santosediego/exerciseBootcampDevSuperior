f316f2487c1b32e13c821ca6c55376752e6410b5
Diego Santos <diegoeustaquiodossantos@hotmail.com>
2020-10-10 07:00:46 -0300

    First commit, project created

---
+++ backend/src/main/java/com/santosediego/dsexercicio/DsexercicioApplication.java
@@ -0,0 +1,13 @@
+package com.santosediego.dsexercicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsexercicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsexercicioApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/santosediego/dsexercicio/DsexercicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santosediego.dsexercicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsexercicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
